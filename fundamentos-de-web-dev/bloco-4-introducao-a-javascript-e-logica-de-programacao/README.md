48912d009b01d3cd2b6cf9aa38e86c249561423d
Jesse <jessmed19@gmail.com>
2022-05-12 18:45:27 -0300

    Adiciona nova pasta com bloco 4

---
+++ fundamentos-de-web-dev/bloco-4-introducao-a-javascript-e-logica-de-programacao/README.md
@@ -0,0 +1,1 @@
+Pasta contendo todos exercicios do bloco 4 (Introdução à JavaScript e Lógica Programacional) do módulo de fundamentos do curso de web develop da Trybe.
---
+++ fundamentos-de-web-dev/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -0,0 +1,24 @@
+/* Exercício 01
+
+const myName = 'Carolina';
+const birthCity = 'São Paulo';
+let birthYear = 1996;
+
+birthYear = 2030;
+birthCity = 'Juazeiro';
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)*/
+
+//----------------------------------------
+
+// Exercício 02
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
