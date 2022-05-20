50a858b086bb783adfb85a2192b0456090af1ca1
Jesse <jessmed19@gmail.com>
2022-05-20 18:43:29 -0300

    Adiciona exercícios de fixação

---
+++ fundamentos-de-web-dev/bloco-5-javascript-DOM-eventos-e-web-storage/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.backgroundColor = 'rgb(100, 0, 300 ';
+
+document.querySelector('#primeiroFilhoDoFilho').innerText = 'Nao tenho ainda, mas terei um dia';
+
+document.querySelector('Pai').childNodes;
+
+document.querySelector('elementoOndeVoceEsta')
+
+// Exercícios de reforço
